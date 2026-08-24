b3b4cb7a91d4d37680688d91115e3c65d22c93f1
CruzMo <moleoni.love@gmail.com>
2026-08-24 11:15:58 +0700

    add lab02 up up

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Lab02
+﻿/*
+ * Student ID : 1690700701
+ * Name       : Thampapon Thuamboribun
+ * Section    : 129A
+ * No.        : 0
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Lab02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level:{level}");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            Console.WriteLine();
+
+            int hpPercet = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent; {hpPercet}%");
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damage!");
+            Console.WriteLine();
+            Console.WriteLine($"===== BOSS STATUS: AFTER DAMAGE =====");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            Console.WriteLine($"{currentHp * 100 / maxHp}");
+
         }
     }
 }
