aa998300b1247d8c1181113c2ef208cf03a1aa45
CruzMo <moleoni.love@gmail.com>
2026-08-25 22:32:27 +0700

    Update Program.cs

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -41,7 +41,85 @@ namespace Lab02
             Console.WriteLine($"HP: {currentHp} / {maxHp}");
             hpPercent = currentHp * 100 / maxHp;
             Console.WriteLine($"HP Percent: {hpPercent}%");
-            Console.ReadKey();
+            
+            Console.WriteLine();
+            Console.WriteLine($"===== PART B : MY RPG CHARACTERS =====");
+            Console.WriteLine();
+
+            // ===== Knight =====
+            string knightName = "George";
+            int knightLevel = 12;
+            int kinghtHp = 320;
+            float knightAttack = 48.5f;
+            float knightDefense = 96.9f;
+            bool knightIsTank = true;
+
+            Console.WriteLine($"KNIGHT");
+            Console.WriteLine($"Name: {knightName}");
+            Console.WriteLine($"Level: {knightLevel}");
+            Console.WriteLine($"HP: {kinghtHp}");
+            Console.WriteLine($"Attack Power: {knightAttack}");
+            Console.WriteLine($"Defense: {knightDefense}");
+            Console.WriteLine($"Is Tank:{knightIsTank}");
+            Console.WriteLine();
+
+            // ===== Mage =====
+            string mageName = "Nox";
+            int mageLevel = 14;
+            int mageHp = 200;
+            float mageMagic = 92.0f;
+            double mageCrit = 2.15;
+            bool mageIsBoss = false;
+
+            Console.WriteLine($"MAGE");
+            Console.WriteLine($"Name: {mageName}");
+            Console.WriteLine($"Level: {mageLevel}");
+            Console.WriteLine($"HP: {mageHp}");
+            Console.WriteLine($"Magic Power: {mageMagic}");
+            Console.WriteLine($"Critical Multiplier: {mageCrit}");
+            Console.WriteLine($"Is Boss: {mageIsBoss}");
+            Console.WriteLine();
+
+            // ===== Archer =====
+            string archerName = "Sylvan";
+            int archerLevel = 11;
+            int archerHp = 210;
+            float archerSpeed = 38.7f;
+            char archerRank = 'A';
+            bool archerIsHasPet = true;
+
+            Console.WriteLine($"ARCHER");
+            Console.WriteLine($"Name: {archerName}");
+            Console.WriteLine($"Level: {archerLevel}");
+            Console.WriteLine($"HP: {archerHp}");
+            Console.WriteLine($"Speed: {archerSpeed}");
+            Console.WriteLine($"Rank: {archerRank}");
+            Console.WriteLine($"Has Pet: {archerIsHasPet}");
+            Console.WriteLine();
+
+            // ===== Assassin =====
+            String assassinName = "Ronan";
+            int assassinLevel = 15;
+            int assassinHp = 180;
+            float assassinAttack = 55.3f;
+            double assassinCrit = 2.45;
+            bool assassinIsStealth = true;
+
+            Console.WriteLine($"ASSASSIN");
+            Console.WriteLine($"Name: {assassinName}");
+            Console.WriteLine($"Level: {assassinLevel}");
+            Console.WriteLine($"HP: {assassinHp}");
+            Console.WriteLine($"Attack Power: {assassinAttack}");
+            Console.WriteLine($"Critical Multiplier: {assassinCrit}");
+            Console.WriteLine($"Is Stealth: {assassinIsStealth}");
+            Console.WriteLine();
+            Console.ReadLine();
         }
+
+
+
+
+
     }
-}
+    }
+
